448fc090da82c04d72e0bffc0d9445a12d99bf24
Ava68 <63304698+Ava68@users.noreply.github.com>
2020-10-16 01:02:27 +0530

    deploying

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.post("/", function (req, res) {
 })
 
 
-app.listen(4000, function () {
+app.listen(process.env.PORT || 4000, function () {
     console.log("Server is running on port 4000");
 })
 
